c0d9130f215db0d929aacdf6aadb7885d8af403a
adamissam <adamissam>
2022-02-21 16:58:17 +0100

    harmonization of font between ios and android

--- components/PointOfSale/index.js
+++ components/PointOfSale/index.js
@@ -28,7 +28,6 @@ const styles=StyleSheet.create({
         color:colors.primary,
         fontWeight:'bold',
         fontSize:17,
-        fontFamily:'SpaceMono'
     },
     textPosition:{
         color:colors.text,
